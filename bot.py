84339f47d4bd815ae009760711b306711529f2fc
SDM <steamdeckmods@gmail.com>
2021-07-25 14:44:10 -0400

    Added scoopnotification command

--- bot.py
+++ bot.py
@@ -2,14 +2,24 @@ import discord
 from discord.ext import commands
 import config
 
-client = commands.Bot(command_prefix=config.prefix)
+bot = commands.Bot(command_prefix=config.prefix)
 
-@client.event
+@bot.event
 async def on_ready():
-    print("Steam Discord bot operational.")
+    print("Steam Deck bot operational.")
 
-@client.command()
-async def hello(ctx):
-    await ctx.send("Howdy")
+@bot.command()
+async def scoopnotification(ctx):
+    if(isResearcher(ctx)) and ctx.message.channel.id == config.research_channel:
+        await ctx.send("Notified News Junkies in the Scoop channel.")
+        await bot.get_channel(config.scoop_channel).send("<@&" + str(config.newsjunkie_role) + "> - a scoop has been posted below.")
+
+def isHelper(ctx):
+    targetrole = discord.utils.get(ctx.message.guild.roles, id=config.helper_role)
+    return targetrole in ctx.message.author.roles
+
+def isResearcher(ctx):
+    targetrole = discord.utils.get(ctx.message.guild.roles, id=config.researcher_role)
+    return targetrole in ctx.message.author.roles
     
-client.run(config.token)
+bot.run(config.token)
